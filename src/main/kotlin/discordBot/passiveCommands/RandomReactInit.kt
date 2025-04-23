4f3dbdaba1e3afba590aae0edf9595674d2dc416
matkijahenkilo <matkija.henkilo@gmail.com>
2025-04-23 13:30:30 -0300

    Lowercase message to check easier if tsih or nora got mentioned

--- src/main/kotlin/discordBot/passiveCommands/RandomReactInit.kt
+++ src/main/kotlin/discordBot/passiveCommands/RandomReactInit.kt
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 fun randomReactInit(jda: JDA) {
     jda.listener<MessageReceivedEvent> { event ->
         if (event.author.id == event.jda.selfUser.id || event.author.isBot) return@listener
-        val c = event.message.contentRaw
+        val c = event.message.contentRaw.lowercase()
 
         if (Math.random() <= 0.02 || c.contains("tsih") || c.contains("nora")) {
             val customEmoji = jda.guilds.random().emojis.random()
