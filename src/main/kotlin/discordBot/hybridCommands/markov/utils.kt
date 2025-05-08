d4bd6b71359b5dcd5045ad734bc3fa75f47b0534
matkijahenkilo <matkija.henkilo@gmail.com>
2025-05-08 10:29:53 -0300

    Correct variable name

--- src/main/kotlin/discordBot/hybridCommands/markov/utils.kt
+++ src/main/kotlin/discordBot/hybridCommands/markov/utils.kt
@@ -2,14 +2,14 @@ package org.matkija.bot.discordBot.hybridCommands.markov
 
 private val linkPatters = Regex("<\\bhttps://[^\\s>]+(?=>)>|\\bhttps://\\S+|<\\bhttp://[^\\s>]+(?=>)>|\\bhttp://\\S+")
 private val mentionPattern = Regex("<@[0-9]+>")
-private val doubleSpacePattern = Regex("\\s+")
+private val spacePattern = Regex("\\s+")
 
 private fun String.filterOutLinks(): String = this.replace(linkPatters, "")
 private fun String.filterOutMentionPatterns(): String = this.replace(mentionPattern, "")
-private fun String.makeDoubleSpacePatternsJustOneSpace(): String = this.replace(doubleSpacePattern, " ")
+private fun String.replaceDoubleSpacesIntoSingularSpace(): String = this.replace(spacePattern, " ")
 
 fun String.clearForMarkovCorpus(): String = this
     .replace("\n", " ")
     .filterOutLinks()
     .filterOutMentionPatterns()
-    .makeDoubleSpacePatternsJustOneSpace()
+    .replaceDoubleSpacesIntoSingularSpace()
