ce57aab34c5a80dbe69cf0db902548dd622ad53c
matkijahenkilo <matkija.henkilo@gmail.com>
2025-05-31 16:47:00 -0300

    Change default download limit to 10 imgs
    also refactor that file

--- src/main/kotlin/discordBot/passiveCommands/sauceSender/gallerydlHandler.kt
+++ src/main/kotlin/discordBot/passiveCommands/sauceSender/gallerydlHandler.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.coroutineScope
 import net.dv8tion.jda.api.entities.MessageEmbed
 import java.io.File
 
+private const val defaultLimit = 10
+
 fun spawnGallerydlProcess(command: List<String>): Process = ProcessBuilder(command)
     .directory(File("./data"))
     .redirectOutput(ProcessBuilder.Redirect.PIPE)
@@ -23,19 +25,19 @@ val baseArgs = listOf("gallery-dl", "--cookies", "./cookies.txt")
 fun filterOutWords(content: String): List<String> =
     content.split(" ").filter { it.contains("https://") }.toList()
 
-fun makeTwitterCommand(link: String, shouldGetInfoOnly: Boolean = false, limit: Int = 5): List<String> =
+fun makeTwitterCommand(link: String, shouldGetInfoOnly: Boolean = false, limit: Int = defaultLimit): List<String> =
     baseArgs + if (shouldGetInfoOnly) infoArgs(link, twitterFilters) else downloadArgs(link, limit)
 
-fun makeMisskeyCommand(link: String, shouldGetInfoOnly: Boolean = false, limit: Int = 5): List<String> =
+fun makeMisskeyCommand(link: String, shouldGetInfoOnly: Boolean = false, limit: Int = defaultLimit): List<String> =
     baseArgs + if (shouldGetInfoOnly) infoArgs(link, misskeyFilters) else downloadArgs(link, limit)
+
+fun makeCommonCommand(link: String, limit: Int = defaultLimit) = baseArgs + downloadArgs(link, limit)
 
 fun downloadArgs(link: String, limit: Int): List<String> =
     listOf("--range", "1-$limit", "--ugoira-conv", "-D", "./temp/", link)
 
 fun infoArgs(link: String, filter: String): List<String> =
     listOf("--filter", filter, "-s", link)
-
-fun makeCommonCommand(link: String) = baseArgs + downloadArgs(link, 5)
 
 fun makeMediaCheckCommand(link: String): List<String> =
     baseArgs + listOf("-g", "-s", link)
