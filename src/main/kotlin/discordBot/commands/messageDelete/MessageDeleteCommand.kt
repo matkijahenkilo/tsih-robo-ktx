af0d9a1bddc6ec026fd50424f252b2f9daa4fd5b
matkijahenkilo <matkija.henkilo@gmail.com>
2025-05-31 16:30:10 -0300

    Add message command to delete bot's own message

--- src/main/kotlin/discordBot/commands/messageDelete/MessageDeleteCommand.kt
+++ src/main/kotlin/discordBot/commands/messageDelete/MessageDeleteCommand.kt
@@ -1,10 +1,10 @@
-package org.matkija.bot.discordBot.passiveCommands.sauceSender
+package org.matkija.bot.discordBot.commands.messageDelete
 
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
-object SauceSenderCommands {
+object MessageDeleteCommand {
     const val MESSAGE_COMMAND_DELETE_MESSAGE = "delete tsih's message"
-    
+
     fun getCommands(): CommandData = Commands.message(MESSAGE_COMMAND_DELETE_MESSAGE)
 }
--- src/main/kotlin/discordBot/passiveCommands/sauceSender/sauceSenderInit.kt
+++ src/main/kotlin/discordBot/passiveCommands/sauceSender/sauceSenderInit.kt
@@ -3,15 +3,18 @@ package org.matkija.bot.discordBot.passiveCommands.sauceSender
 import dev.minn.jda.ktx.events.listener
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import org.matkija.bot.LOG
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 
 fun sauceSenderInit(jda: JDA) {
 
-    if (!File("data/cookies.txt").exists())
-        LOG.warn("cookies.txt doesn't exist in data/, many websites will break if you don't have your cookies exported!")
+    val logger: Logger = LoggerFactory.getLogger("sauceSenderInit")
 
-    LOG.info("Clearing data/temp folder")
+    if (!File("data/cookies.txt").exists())
+        logger.warn("cookies.txt doesn't exist in data/, many websites will break if you don't have your cookies exported!")
+
+    logger.info("Clearing data/temp folder")
     File("./data/temp").listFiles()?.forEach { it.delete() }
 
     jda.listener<MessageReceivedEvent> { event ->
@@ -22,7 +25,7 @@ fun sauceSenderInit(jda: JDA) {
             try {
                 SauceSender(event, c).sendSauce()
             } catch (e: Exception) {
-                LOG.error(e.toString())
+                logger.error(e.toString())
             }
         }
     }
