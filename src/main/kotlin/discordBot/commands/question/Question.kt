cf8e14439219baa95b24a3178ccba8cc61f2a864
matkijahenkilo <matkija.henkilo@gmail.com>
2025-10-07 19:31:38 -0300

    Remove non affirmative replies from Question

--- src/main/kotlin/discordBot/commands/question/Question.kt
+++ src/main/kotlin/discordBot/commands/question/Question.kt
@@ -4,21 +4,19 @@ import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionE
 import org.matkija.bot.discordBot.abstracts.SlashCommand
 
 private val answers = listOf(
-    "Of course!",
+    "Of course nora!",
     "That's a strong yes nanora!",
     "Indeed nora!",
     "Ooooh!! Yes nora!!!",
     "Hmmmm... I guess nanora?",
     "Yes, that's based nanora!",
+
     "I don't think so nora.",
-    "Well, yes, but no nanora.",
-    "I think that perhaps it's a maybe nanora!",
-    "Ask Nanako, won't you nora?!",
-    "Teheheh~ no.",
+    "Teheheh~ no nanora.",
     "I think it's better not to answer nora.",
     "No, that's cringe nanora!",
-    "You just invented these words nanora.",
-    "Whooooa! n-no, nora!",
+    "Hell no, nanora.",
+    "Of course not nanora!"
 )
 
 class Question(private val event: GenericCommandInteractionEvent) : SlashCommand(event) {
