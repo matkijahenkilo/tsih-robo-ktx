e4d6d7bac2098d0641bf8ebf7d61ce9f3f6ac3c4
matkijahenkilo <matkija.henkilo@gmail.com>
2026-01-29 10:17:24 -0300

    Make Avatar command also fetch both members and users avatars

--- src/main/kotlin/discordBot/commands/avatar/Avatar.kt
+++ src/main/kotlin/discordBot/commands/avatar/Avatar.kt
@@ -4,17 +4,19 @@ import dev.minn.jda.ktx.messages.EmbedBuilder
 import dev.minn.jda.ktx.messages.reply_
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
 import org.matkija.bot.discordBot.abstracts.SlashCommand
+import org.matkija.bot.utils.getRandomColor
 
 class Avatar(private val event: GenericCommandInteractionEvent) : SlashCommand(event) {
     override fun execute() {
-        val user = event.getOption(AvatarSlashOptions.AVATAR_OPTION_USER)
+        val member = event.getOption(AvatarSlashOptions.AVATAR_OPTION_MEMBER)?.asMember
+        val asUser = event.getOption(AvatarSlashOptions.AVATAR_OPTION_AS_USER)?.asBoolean ?: false
         val avatarUrl =
-            (if (user?.asUser != null) user.asUser.effectiveAvatarUrl else event.user.effectiveAvatarUrl) + "?size=2048"
+            (if (asUser) member?.user?.effectiveAvatarUrl else member?.effectiveAvatarUrl ?: event.user.effectiveAvatarUrl) + "?size=2048"
         event.reply_(
             embeds = listOf(
                 EmbedBuilder {
                     title = "This is a nice avatar nanora!"
-                    color = 0xff80fd
+                    color = getRandomColor()
                     field {
                         name = "What a lazy adult!"
                         value = "They look like holding a lot of shiny stars..."
